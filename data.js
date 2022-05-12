dc42f9076a22f4177462b7e792b310702da9853b
usernameyuan <lowkaivuan@gmail.com>
2022-05-12 15:07:37 +0800

    First Commit
    My First commit of tour TEL_360 webimages

---
+++ data.js
@@ -0,0 +1,158 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.1249203069477609,
+        "pitch": -0.023726873670714355,
+        "fov": 1.2723536005922274
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8108386894337265,
+          "pitch": 0.028968941357405242,
+          "rotation": 0.7853981633974483,
+          "target": "2-engineering-auditorium-outside"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8369034429047844,
+          "pitch": -0.09542053775270709,
+          "title": "Towards Engineering Auditorium Outside&nbsp;<div><br></div>",
+          "text": "Click to move to location&nbsp;<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 3.007850802870289,
+        "pitch": -0.47968870256652707,
+        "fov": 1.2723536005922274
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.127563027960674,
+          "pitch": -0.2494281223954129,
+          "rotation": 0,
+          "target": "2-engineering-auditorium-outside"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.8224230657926697,
+          "pitch": -0.480182952182437,
+          "title": "Engineering Auditorium Outside&nbsp;",
+          "text": "Click to move to location&nbsp;<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-engineering-auditorium-outside",
+      "name": "Engineering Auditorium Outside",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.049965688327477054,
+        "pitch": -0.2669273287955747,
+        "fov": 1.2723536005922274
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8625930488487956,
+          "pitch": 0.046008786546382296,
+          "rotation": 3.9269908169872414,
+          "target": "0-along-engineering-drive-1"
+        },
+        {
+          "yaw": 0.09385974937204011,
+          "pitch": -0.04984773168261292,
+          "rotation": 0,
+          "target": "1-engineering-auditorium-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.30075720955262497,
+          "pitch": -0.19940339079007963,
+          "title": "Towards Engineering Auditorium",
+          "text": "Click to move to location&nbsp;<div><br></div>"
+        },
+        {
+          "yaw": -0.927422064076941,
+          "pitch": -0.07065703554104275,
+          "title": "Towards Along engineering Drive&nbsp;",
+          "text": "Click to move to location&nbsp;<div><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Engineering_KY",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
